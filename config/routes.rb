429772e1e5702584f6ab482d1bfd011e781d5950
Saulo Filho <32524964+saulofilho@users.noreply.github.com>
2021-05-28 17:11:15 -0300

    [fix] routes api v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-  resources :bands
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :bands # /bands
+
+  namespace :api do
+    namespace :v1 do
+      resources :bands do
+        resources :members
+      end
+    end
+
+    namespace :v2 do
+      resources :members
+      resources :bands
+    end
+  end
 end
